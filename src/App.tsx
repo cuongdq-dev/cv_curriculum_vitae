158e17373d5d278064869c6d7e5a3d08da7d23a9
dqcuong <dqcuong19@gmail.com>
2024-10-31 23:41:15 +0700

    Update index html, jquery and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css'
 import { About, Footer, Header, Portfolio, Resume, Testimonials } from './components/index'
 import { resumeData } from './constants'
-import './styles/default.css'
-import './styles/fonts.css'
-import './styles/layout.css'
-import './styles/magnific-popup.css'
-import './styles/media-queries.css'
+
 
 function App() {
 
